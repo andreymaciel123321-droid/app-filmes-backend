607f51b095485be34588e005fda092117f31452b
andreymaciel123321-droid <andreymaciel123321@gmail.com>
2025-11-28 10:40:32 -0300

    Update TabRoutes.js

--- mobile/navigation/TabRoutes.js
+++ mobile/navigation/TabRoutes.js
@@ -1,1 +1,45 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabRoutes() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: '#000',
+          borderTopColor: '#000',
+        },
+        tabBarActiveTintColor: '#fff',
+        tabBarInactiveTintColor: '#888',
+      }}
+    >
+      <Tab.Screen
+        name="Início"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Perfil"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-circle" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+        }
+          
