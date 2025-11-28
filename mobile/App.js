8cefd6b1b9097604141a8d6335f4b123bc6d915e
andreymaciel123321-droid <andreymaciel123321@gmail.com>
2025-11-27 22:24:36 -0300

    Create App.js

---
+++ mobile/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import StackRoutes from './navigation/StackRoutes';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StackRoutes />
+    </NavigationContainer>
+  );
+}
