5d5cef564f55b84d972a337bfe6c7f62b9c3e9f2
andreymaciel123321-droid <andreymaciel123321@gmail.com>
2025-11-28 19:10:14 -0300

    Update PlayerScreen.js

--- mobile/screens/PlayerScreen.js
+++ mobile/screens/PlayerScreen.js
@@ -1,1 +1,53 @@
+import React, { useRef, useState } from "react";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+import { Video, ResizeMode } from "expo-av";
 
+export default function PlayerScreen({ route }) {
+  const { videoUrl } = route.params; // URL enviada pela DetailsScreen
+  const videoRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <View style={styles.container}>
+      <Video
+        ref={videoRef}
+        style={styles.video}
+        source={{ uri: videoUrl }}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        onLoadStart={() => setIsLoading(true)}
+        onLoad={() => setIsLoading(false)}
+        shouldPlay
+      />
+
+      {isLoading && (
+        <View style={styles.loading}>
+          <ActivityIndicator size="large" color="#fff" />
+        </View>
+      )}
+    </View>
+  ); 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+  },
+  loading: {
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
+          
