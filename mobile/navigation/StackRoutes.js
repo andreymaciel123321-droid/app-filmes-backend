0d0af5592ed97188bec1b208057e175d2fa24957
andreymaciel123321-droid <andreymaciel123321@gmail.com>
2025-11-28 10:50:20 -0300

    Update StackRoutes.js

--- mobile/navigation/StackRoutes.js
+++ mobile/navigation/StackRoutes.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+
 import TabRoutes from './TabRoutes';
 import DetailsScreen from '../screens/DetailsScreen';
 import PlayerScreen from '../screens/PlayerScreen';
@@ -9,27 +12,21 @@ const Stack = createNativeStackNavigator();
 
 export default function StackRoutes() {
   return (
-    <Stack.Navigator>
-      {/* Rotas principais */}
-      <Stack.Screen
-        name="TabRoutes"
-        component={TabRoutes}
-        options={{ headerShown: false }}
-      />
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
 
-      {/* Tela de detalhes */}
-      <Stack.Screen
-        name="Details"
-        component={DetailsScreen}
-        options={{ headerShown: false }}
-      />
+      {/* Telas de Autenticação */}
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+
+      {/* Telas principais */}
+      <Stack.Screen name="Tabs" component={TabRoutes} />
+
+      {/* Detalhes de filme */}
+      <Stack.Screen name="Details" component={DetailsScreen} />
 
       {/* Player */}
-      <Stack.Screen
-        name="Player"
-        component={PlayerScreen}
-        options={{ headerShown: false }}
-      />
+      <Stack.Screen name="Player" component={PlayerScreen} />
+
     </Stack.Navigator>
   );
 }
