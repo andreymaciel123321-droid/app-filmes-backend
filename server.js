a90a6854997ab20c742e1cbcf4d57299d0dbec0c
andreymaciel123321-droid <andreymaciel123321@gmail.com>
2025-11-29 15:24:44 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API do app de filmes está rodando!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Servidor rodando na porta " + PORT));
