1877b10595ed14fae712ec424cdcc4040b906536
andreymaciel123321-droid <andreymaciel123321@gmail.com>
2025-11-29 15:39:17 -0300

    Update api.js

--- mobile/services/api.js
+++ mobile/services/api.js
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://backend.onrender.com", // sua URL do backend
+  baseURL: 'https://filmes-api-0yfr.onrender.com', // sua API no Render
+  timeout: 10000, // 10 segundos (evita travar)
 });
 
 export default api;
