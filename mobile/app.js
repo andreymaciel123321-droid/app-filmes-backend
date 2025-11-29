078cf83f38e258f733913cc6d9d43a7d87ef4e8e
andreymaciel123321-droid <andreymaciel123321@gmail.com>
2025-11-29 09:20:28 -0300

    Create app.js

---
+++ mobile/app.js
@@ -0,0 +1,23 @@
+{
+  "expo": {
+    "name": "SeuAppDeFilmes",
+    "slug": "seu-app-de-filmes",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#000000"
+    },
+    "assetBundlePatterns": ["**/*"],
+    "android": {
+      "package": "com.seuusuario.seuappdefilmes",
+      "versionCode": 1,
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/icon.png",
+        "backgroundColor": "#000000"
+      }
+    }
+  }
+        }
