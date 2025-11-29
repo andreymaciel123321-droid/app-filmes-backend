e7ab2e451d2322a6ef95cba288e25a7400ea3af4
andreymaciel123321-droid <andreymaciel123321@gmail.com>
2025-11-29 09:16:14 -0300

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import StackRoutes from './navigation/StackRoutes';
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <StackRoutes />
-    </NavigationContainer>
-  );
-}
+{
+  "expo": {
+    "name": "SeuAppDeFilmes",
+    "slug": "seu-app-de-filmes",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#000000"
+    },
+    "assetBundlePatterns": ["**/*"],
+    "android": {
+      "package": "com.seuusuario.seuappdefilmes",
+      "versionCode": 1,
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/icon.png",
+        "backgroundColor": "#000000"
+      }
+    }
+  }
+          }
