ba7b75b142c2defcf96e16904a706bbfb99e97ce
andreymaciel123321-droid <andreymaciel123321@gmail.com>
2025-11-29 23:55:26 -0300

    Update PlayerScreen.js

--- mobile/screens/PlayerScreen.js
+++ mobile/screens/PlayerScreen.js
@@ -1,53 +1,13 @@
-import React, { useRef, useState } from "react";
-import { View, StyleSheet, ActivityIndicator } from "react-native";
-import { Video, ResizeMode } from "expo-av";
+import React from "react";
+import { View } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function PlayerScreen({ route }) {
-  const { videoUrl } = route.params; // URL enviada pela DetailsScreen
-  const videoRef = useRef(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const { link } = route.params;
 
   return (
-    <View style={styles.container}>
-      <Video
-        ref={videoRef}
-        style={styles.video}
-        source={{ uri: videoUrl }}
-        useNativeControls
-        resizeMode={ResizeMode.CONTAIN}
-        onLoadStart={() => setIsLoading(true)}
-        onLoad={() => setIsLoading(false)}
-        shouldPlay
-      />
-
-      {isLoading && (
-        <View style={styles.loading}>
-          <ActivityIndicator size="large" color="#fff" />
-        </View>
-      )}
+    <View style={{ flex: 1 }}>
+      <WebView source={{ uri: link }} />
     </View>
-  ); 
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  video: {
-    width: "100%",
-    height: "100%",
-  },
-  loading: {
-    position: "absolute",
-    justifyContent: "center",
-    alignItems: "center",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-});
-          
