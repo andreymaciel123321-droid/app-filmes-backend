6522011212539aac33a2ec15f3804bbb687a9b7a
andreymaciel123321-droid <andreymaciel123321@gmail.com>
2025-11-29 23:57:56 -0300

    Update App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import Routes from './src/routes';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import HomeScreen from "./screens/HomeScreen";
+import DetailsScreen from "./screens/DetailsScreen";
+import PlayerScreen from "./screens/PlayerScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Routes />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Player" component={PlayerScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-}
+  }
